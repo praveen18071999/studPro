de8cecf988ca1156f7b8e793d03756204e7d9d2b
Praveen Kumar Gangapuram <pgangapu@buffalo.edu>
2025-01-15 23:32:24 -0500

    inital commit

---
+++ frontend/studpro/.next/static/chunks/app_problem_[id]_page_tsx_6fa2a3._.js
@@ -0,0 +1,18 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_problem_[id]_page_tsx_6fa2a3._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_problem_[id]_page_tsx_6fa2a3._.js",
+  "chunks": [
+    "static/chunks/node_modules_@codemirror_state_dist_index_9b015e.js",
+    "static/chunks/node_modules_@codemirror_view_dist_index_201017.js",
+    "static/chunks/node_modules_@radix-ui_840c2a._.js",
+    "static/chunks/node_modules_@floating-ui_9ec1fa._.js",
+    "static/chunks/node_modules_@lezer_7aa779._.js",
+    "static/chunks/node_modules_@codemirror_199a6c._.js",
+    "static/chunks/node_modules_9a1f05._.js",
+    "static/chunks/_16726e._.js"
+  ],
+  "source": "dynamic"
+});
