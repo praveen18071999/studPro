4b08a67fde8c826d67e93bbda6b33412dd50206f
Praveen Kumar Gangapuram <pgangapu@buffalo.edu>
2025-01-17 00:30:46 -0500

    initial commit

--- frontend/studpro/components/ui/navbar.tsx
+++ frontend/studpro/components/ui/navbar.tsx
@@ -64,7 +64,7 @@ export default function Navbar() {
                   <Link href="/userProfile" className="block px-4 py-2 text-sm text-gray-700 dark:text-white" role="menuitem">
                     Profile
                   </Link>
-                  <button onClick={()=>{localStorage.removeItem('access_token');window.location.href='/authentication'}} className="block w-full text-left px-4 py-2 text-sm text-gray-700 dark:text-white" role="menuitem">
+                  <button onClick={()=>{localStorage.removeItem('access_token');localStorage.setItem('theme','light');window.location.href='/authentication'}} className="block w-full text-left px-4 py-2 text-sm text-gray-700 dark:text-white" role="menuitem">
                     <LogOut className="mr-3 h-5 w-5" />
                     Sign out
                   </button>
@@ -115,7 +115,7 @@ export default function Navbar() {
               <Link href="/userProfile" className="block px-3 py-2 rounded-md text-base font-medium text-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700">
                 Profile
               </Link>
-              <button onClick={()=>{localStorage.removeItem('access_token');window.location.href='/authentication'}} className="flex items-center w-full text-left px-3 py-2 rounded-md text-base font-medium text-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700">
+              <button onClick={()=>{localStorage.removeItem('access_token');localStorage.setItem('theme','light');window.location.href='/authentication'}} className="flex items-center w-full text-left px-3 py-2 rounded-md text-base font-medium text-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700">
                 <LogOut className="mr-3 h-5 w-5" />
                 Sign out
               </button>
