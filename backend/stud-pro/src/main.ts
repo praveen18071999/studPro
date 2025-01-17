a7b51acd820baa4d23e31229ef80239e14f9b841
Praveen Kumar Gangapuram <pgangapu@buffalo.edu>
2025-01-16 22:25:11 -0500

    initial

--- backend/stud-pro/src/main.ts
+++ backend/stud-pro/src/main.ts
@@ -3,7 +3,18 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const allowedOrigins = [
+    "http://localhost:3000",
+    "http://167.172.155.139",
+    "http://167.172.155.139/",
+    "https://167.172.155.139",
+    "https://167.172.155.139/",
+  ];
+  app.enableCors({
+    origin: allowedOrigins,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+  });
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
