7d3aace07f56e0976c63135bd73218492b47f88a
Praveen Kumar Gangapuram <pgangapu@buffalo.edu>
2025-01-18 04:46:50 -0500

    initil commit

--- backend/stud-pro/src/flowChart/flowChart.service.ts
+++ backend/stud-pro/src/flowChart/flowChart.service.ts
@@ -5,7 +5,7 @@ import { geminiService } from 'src/gemini.service';
 export class FlowChartService {
   constructor(private readonly GeminiService: geminiService) {}
   async flowChartCode(code: string) {
-    const prompt = `Convert the code to JavaScript and only give me the code with no Explanation, and compilation errors This code should be used for SVG. code:${code}`;
+    const prompt = `Convert the following code to JavaScript, specifically for SVG conversion in js2flowchart. Provide only the converted code with no explanations or additional comments. code:${code}`;
     const response = await this.GeminiService.geminiPrompt(prompt);
     return { success: true, actualOutput: response.actualOutput };
   }
